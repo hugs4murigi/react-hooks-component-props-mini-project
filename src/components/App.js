f1906d69385f32d1c2df71bee205c3d61fa07455
brian <brianmurigi19@gmail.com>
2023-11-09 22:36:47 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header"
+import About from "./About"
+import ArticleList from "./Articlelist";
 
 console.log(blogData);
 
 function App() {
+  const blogName = "My Awesome Blog";
+  const aboutText = "MY FIRST react blog!"
+  const posts = [
+    { id: 1, title: "Post 1", content: "This is the content of Post 1." },
+    { id: 2, title: "Post 2", content: "This is the content of Post 2." },
+    // Add more post objects as needed
+  ];
+
+
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header blogName={blogName}/>
+      <About
+      aboutText={aboutText}
+      imageSrc={''}/>
+      <ArticleList posts={posts} />
+      {/* Rest of your App components */}
+    
+      
     </div>
   );
 }
